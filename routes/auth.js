c0a6b46a5e66a68a1fdc0f3fec0c5b15695ef17c
Avisa Poshtkouhi <avisa@omniscent.com>
2024-12-22 20:52:47 -0800

    feat: signout route

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,6 @@ router.get("/auth/sign-up", authCtrl.signUp);
 router.post('/auth/sign-up', authCtrl.signUpPost);
 router.get("/auth/sign-in", authCtrl.signIn);
 router.post("/auth/sign-in", authCtrl.signinPost);
-
+router.get("/auth/sign-out", authCtrl.signout);
 
 module.exports = router;
