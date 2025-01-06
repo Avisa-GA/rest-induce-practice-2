42e20a15f64e6001ac9676c6408c4b2ba47131a4
Avisa Poshtkouhi <avisa@omniscent.com>
2025-01-05 19:14:50 -0800

    feat: add comment model and add to book

--- models/book.js
+++ models/book.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
+const commentSchema = new mongoose.Schema({
+    content: String,
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
 const bookSchema = new mongoose.Schema({
     title: String,
     author: String,
+    comments: [commentSchema],
     createdBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
